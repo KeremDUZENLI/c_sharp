34cd833aacaf88b0049534ade2902d87fd1026d0
kerem.duzenli <krmdznl97@gmail.com>
2023-01-30 19:02:33 -0300

    2023.01.30

---
+++ C SHARP Tutorial (Engin DEMIROG)/CSharpCourse/Conditionals/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+
+            switch (number)
+            {
+                case 10:
+                    Console.WriteLine("10");
+                    break;
+                case 20:
+                    Console.WriteLine("20");
+                    break;
+                default:
+                    Console.WriteLine("None");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
