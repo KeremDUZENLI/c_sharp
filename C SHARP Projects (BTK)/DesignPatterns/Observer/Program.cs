a4a9ba68293607d02f4342c010e5176708a4630e
kerem.duzenli <krmdznl97@gmail.com>
2023-03-01 19:53:41 -0300

    2023.03.01

---
+++ C SHARP Projects (BTK)/DesignPatterns/Observer/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager pM = new ProductManager();
+
+            var customerObserver = new CustomerObserver();
+            pM.Attach(customerObserver);
+            pM.Detach(customerObserver);
+
+            pM.Attach(new EmployeeObserver());
+
+
+            pM.UpdatePrice();
+
+
+            Console.ReadLine();
+        }
+    }
+
+    class ProductManager
+    {
+        List<Observer> observers = new List<Observer>();
+        public void UpdatePrice()
+        {
+            Console.WriteLine("Product price changed");
+            Notify();
+        }
+
+        public void Attach(Observer observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void Detach(Observer observer)
+        {
+            observers.Remove(observer);
+        }
+
+        private void Notify()
+        {
+            foreach (var i in observers)
+            {
+                i.Update();
+            }
+        }
+    }
+
+    abstract class Observer
+    {
+        public abstract void Update();
+    }
+
+    class CustomerObserver : Observer
+    {
+        public override void Update()
+        {
+            Console.WriteLine("Message to customer : Product price changed");
+        }
+    }
+
+    class EmployeeObserver : Observer
+    {
+        public override void Update()
+        {
+            Console.WriteLine("Message to employee : Product price changed");
+        }
+    }
+}
---
+++ C SHARP Projects (BTK)/DesignPatterns/Strategy/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager cM = new CustomerManager();
+            cM.CreditCalculatorBase = new After2010CreditCalculator();
+            cM.SaveCredit();
+
+            Console.ReadLine();
+        }
+    }
+
+    abstract class CreditCalculatorBase
+    {
+        public abstract void Calculate();
+    }
+
+    class Before2010CreditCalculator : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("before 2010");
+        }
+    }
+
+    class After2010CreditCalculator : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("after 2010");
+        }
+    }
+
+    class CustomerManager
+    {
+        public CreditCalculatorBase CreditCalculatorBase { get; set; }
+        public void SaveCredit()
+        {
+            Console.WriteLine("Customer manager business");
+            CreditCalculatorBase.Calculate();
+        }
+    }
+}
