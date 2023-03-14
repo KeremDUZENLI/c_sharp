18af5954ffe99831a4da16cf8325b6b31b24018c
kerem.duzenli <krmdznl97@gmail.com>
2023-03-14 19:53:19 -0300

    2023.03.14

---
+++ C SHARP Projects (BTK)/DesignPatterns/State/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context = new Context();
+
+            ModifiedState modified = new ModifiedState();
+            modified.DoAction(context);
+
+            DeletedState deleted = new DeletedState();
+            deleted.DoAction(context);
+
+            Console.WriteLine(context.GetState());
+
+
+            Console.ReadLine();
+        }
+    }
+
+    interface IState
+    {
+        void DoAction(Context context);
+    }
+
+    class Context
+    {
+        private IState _state;
+
+        public void SetState(IState state)
+        {
+            _state = state;
+        }
+
+        public IState GetState()
+        {
+            return _state;
+        }
+    }
+
+    class ModifiedState:IState
+    {
+        public void DoAction(Context context)
+        {
+            Console.WriteLine("State : Modified\n");
+            context.SetState(this);
+        }
+    }
+
+    class DeletedState : IState
+    {
+        public void DoAction(Context context)
+        {
+            Console.WriteLine("State : Deleted\n");
+            context.SetState(this);
+        }
+    }
+
+    class AddedState : IState
+    {
+        public void DoAction(Context context)
+        {
+            Console.WriteLine("State : Added\n");
+            context.SetState(this);
+        }
+    }
+}
---
+++ C SHARP Projects (BTK)/DesignPatterns/TemplateMethod/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ScoringAlgorithm algorithm;
+
+            Console.WriteLine("\nMen");
+            algorithm = new MenScoringAlgorithm();
+            Console.WriteLine(algorithm.GenerateScore(10, new TimeSpan(0, 2, 34)));
+
+            Console.WriteLine("\nWomen");
+            algorithm = new WomenScoringAlgorithm();
+            Console.WriteLine(algorithm.GenerateScore(10, new TimeSpan(0, 2, 34)));
+
+            Console.WriteLine("\nChildren");
+            algorithm = new ChildrenScoringAlgorithm();
+            Console.WriteLine(algorithm.GenerateScore(10, new TimeSpan(0, 2, 34)));
+
+
+            Console.ReadLine();
+        }
+    }
+
+    abstract class ScoringAlgorithm
+    {
+        public int GenerateScore(int hits, TimeSpan time)
+        {
+            int score = CalculateBaseScore(hits);
+            int reduction = CalculateReduction(time);
+            return CalculateOverallScore(score, reduction);
+        }
+
+        public abstract int CalculateBaseScore(int hits);
+        public abstract int CalculateReduction(TimeSpan time);
+        public abstract int CalculateOverallScore(int score, int reduction);
+    }
+
+    class MenScoringAlgorithm : ScoringAlgorithm
+    {
+        public override int CalculateBaseScore(int hits)
+        {
+            return hits * 100;
+        }
+
+        public override int CalculateReduction(TimeSpan time)
+        {
+            return (int)time.TotalSeconds / 5;
+        }
+
+        public override int CalculateOverallScore(int score, int reduction)
+        {
+            return score - reduction;
+        }
+    }
+
+    class WomenScoringAlgorithm : ScoringAlgorithm
+    {
+        public override int CalculateBaseScore(int hits)
+        {
+            return hits * 100;
+        }
+
+        public override int CalculateReduction(TimeSpan time)
+        {
+            return (int)time.TotalSeconds / 3;
+        }
+
+        public override int CalculateOverallScore(int score, int reduction)
+        {
+            return score - reduction;
+        }
+    }
+
+    class ChildrenScoringAlgorithm : ScoringAlgorithm
+    {
+        public override int CalculateBaseScore(int hits)
+        {
+            return hits * 100;
+        }
+
+        public override int CalculateReduction(TimeSpan time)
+        {
+            return (int)time.TotalSeconds / 2;
+        }
+
+        public override int CalculateOverallScore(int score, int reduction)
+        {
+            return score - reduction;
+        }
+    }
+}
