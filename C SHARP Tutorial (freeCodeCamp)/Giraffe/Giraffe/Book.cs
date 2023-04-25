576824dac2cee94bdea57cc2d3e511321879ed99
kerem.duzenli <krmdznl97@gmail.com>
2023-04-25 14:00:10 -0300

    2023.04.25

---
+++ C SHARP Tutorial (freeCodeCamp)/Giraffe/Giraffe/Book.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Book
+    {
+        public string _title;
+        public string _author;
+        public int _pages;
+        private string _rating;
+
+        public Book(string title, string author, int pages, string rating)
+        {
+            _title = title;
+            _author = author;
+            _pages = pages;
+            Rating = rating;
+        }
+
+        public bool ReadLong()
+        {
+            if (_pages > 700)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string Rating
+        {
+            get { return _rating; }
+            set
+            {
+                if (value == "low" || value == "mid" || value == "high")
+                {
+                    _rating = value;
+                }
+                else
+                {
+                    _rating = null;
+                }
+            }
+        }
+    }
+}
---
+++ C SHARP Tutorial (freeCodeCamp)/Giraffe/Giraffe/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetBook();
+            ClassStatic.Say("xxx");
+
+            Console.ReadLine();
+        }
+
+        private static void GetBook()
+        {
+            Book book1 = new Book("HP", "J.K. Rowling", 700, "low");
+            Book book2 = new Book("LoR", "Talkien", 800, "xxx");
+
+            Console.WriteLine(book1._author);
+            Console.WriteLine(book2._title);
+
+            Console.WriteLine(book1.ReadLong());
+            Console.WriteLine(book2.ReadLong());
+
+            Console.WriteLine(book1.Rating);
+            Console.WriteLine(book2.Rating);
+        }
+    }
+}
